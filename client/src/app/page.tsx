7c393e1655c04e1c158e684ff035b8798963e070
곽윤호 <kwakdm95@gmail.com>
2023-09-01 23:24:50 +0630

    modify: modularization footer's css code

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { Koulen, Inter } from 'next/font/google'
 import Link from 'next/link'
 
+import styles from '@/app/home.module.css'
+
 const koulen = Koulen({
   subsets: ['latin'],
   weight: ['400']
@@ -13,12 +15,12 @@ const inter = Inter({
 
 export default function Home() {
   return (
-    <div id="main">
-      <div className={`${koulen.className} title`}>
-        <h1 className={'mainTitle'}>studio clip</h1>
-        <h2 className={'subTitle'}>Myanmar</h2>
+    <div id="main" className={styles.main}>
+      <div className={`${koulen.className} ${styles.title}`}>
+        <h1 className={styles.mainTitle}>studio clip</h1>
+        <h2 className={styles.subTitle}>Myanmar</h2>
       </div>
-      <div className={`${inter.className} socialMenu uppercase`}>
+      <div className={`${inter.className} ${styles.socialMenu} uppercase`}>
         <Link href='#'>instar</Link>
         <Link href='https://www.facebook.com/profile.php?id=61550583410087' target='blank'>facebook</Link>
         <Link href='#'>youtube</Link>
