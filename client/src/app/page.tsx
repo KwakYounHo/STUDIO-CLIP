416ac90bfafc07127d9a32a526df907cdd483453
곽윤호 <kwakdm95@gmail.com>
2023-09-01 19:15:45 +0630

    modify: use a varablization text-transform-uppercase

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <h1 className={'mainTitle'}>studio clip</h1>
         <h2 className={'subTitle'}>Myanmar</h2>
       </div>
-      <div className={`${inter.className} socialMenu`}>
+      <div className={`${inter.className} socialMenu uppercase`}>
         <Link href='#'>instar</Link>
         <Link href='https://www.facebook.com/profile.php?id=61550583410087' target='blank'>facebook</Link>
         <Link href='#'>youtube</Link>
