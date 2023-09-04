7231e80598cf19bedea92392374f3f5f79296ce7
곽윤호 <kwakdm95@gmail.com>
2023-09-04 12:39:18 +0630

    fix: use main data, styleing tailwindCSS

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,6 +3,7 @@ import { Koulen, Inter } from 'next/font/google'
 import Link from 'next/link'
 
 import styles from '@/app/home.module.css'
+import { main } from '@/models/main/main'
 
 const koulen = Koulen({
   subsets: ['latin'],
@@ -15,17 +16,18 @@ const inter = Inter({
 
 export default function Home() {
   return (
-    <div id="main" className={styles.main}>
-      <div className={`${koulen.className} ${styles.title}`}>
-        <h1 className={styles.mainTitle}>studio clip</h1>
-        <h2 className={styles.subTitle}>Myanmar</h2>
+    <main className={'w-full h-full flex flex-col items-center justify-center space-y-[4rem] text-white'}>
+      {/* title */}
+      <div className={`${koulen.className} text-center select-none`}>
+        <h1 className={'leading-none text-6xl md:text-8xl'}>studio clip</h1>
+        <h2 className={'leading-none text-4xl tracking-[1.35rem] ml-5 md:text-6xl md:tracking-[2rem] md:ml-7'}>Myanmar</h2>
       </div>
-      <div className={`${inter.className} ${styles.socialMenu} uppercase`}>
-        <Link href='#'>instar</Link>
-        <Link href='https://www.facebook.com/profile.php?id=61550583410087' target='blank'>facebook</Link>
-        <Link href='#'>youtube</Link>
-        <Link href='#'>tiktok</Link>
+      {/* social menu */}
+      <div className={'uppercase flex flex-col items-center md:flex-row text-3xl space-y-5 md:space-x-8 md:space-y-0 md:text-4xl font-bold'}>
+        {main.socialMenu.map(element => {
+          return <Link href={element.url} key={element.title} className={'hover:scale-110 duration-300'}>{element.title}</Link>
+        })}
       </div>
-    </div>
+    </main>
   )
 }
