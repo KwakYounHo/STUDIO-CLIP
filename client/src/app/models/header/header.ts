275b3663e99b366a830094a50c215e5a2340874f
곽윤호 <kwakdm95@gmail.com>
2023-09-04 11:26:27 +0630

    add: add header data file

---
+++ client/src/app/models/header/header.ts
@@ -0,0 +1,58 @@
+export const header = {
+  nav: [
+    {
+      id: 'about',
+      title: 'about us',
+      url: '/about'
+    },
+    {
+      id: '사업분야',
+      title: '사업분야',
+      url: '#'
+    },
+    {
+      id: 'portfolio',
+      title: 'portfolio',
+      url: '/portfolio'
+    },
+    {
+      id: 'contact',
+      title: 'contact us',
+      url: '/contact'
+    },
+  ],
+  social: [
+    {
+      id: 'instargram',
+      blackIcon: '/instar_50px.png',
+      colorIcon: '/instar_color.png',
+      width: 50,
+      height: 50,
+      url: '#'
+    },
+    {
+      id: 'facebook',
+      blackIcon: '/facebook_50px.png',
+      colorIcon: '/facebook_color.png',
+      width: 50,
+      height: 50,
+      url: 'https://www.facebook.com/profile.php?id=61550583410087'
+    },
+    {
+      id: 'youtube',
+      blackIcon: '/youtube_50px.png',
+      colorIcon: '/youtube_color.png',
+      width: 50,
+      height: 50,
+      url: '#'
+    },
+    {
+      id: 'tiktok',
+      blackIcon: '/tiktok_50px.png',
+      colorIcon: '/tiktok_color.png',
+      width: 50,
+      height: 50,
+      url: '#'
+    },
+  ]
+}
