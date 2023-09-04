ea9b74794f391e4ccfb1b7f939d28e859f6635cd
곽윤호 <kwakdm95@gmail.com>
2023-09-04 12:14:05 +0630

    add: create main data

---
+++ client/src/models/main/main.ts
@@ -0,0 +1,20 @@
+export const main = {
+  socialMenu : [
+    {
+      title: 'instargram',
+      url: '#'
+    },
+    {
+      title: 'facebook',
+      url: 'https://www.facebook.com/profile.php?id=61550583410087'
+    },
+    {
+      title: 'youtube',
+      url: '#'
+    },
+    {
+      title: 'tiktok',
+      url: '#'
+    },
+  ]
+}
