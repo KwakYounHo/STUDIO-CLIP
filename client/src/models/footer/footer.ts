6bc00fa07c4cd8f6babd8dbd5c7698b2a15753a3
곽윤호 <kwakdm95@gmail.com>
2023-09-04 11:49:18 +0630

    add: add footer data file

---
+++ client/src/models/footer/footer.ts
@@ -0,0 +1,10 @@
+export const footer = {
+  description: {
+    title: 'STUDIO CLIP Corp.',
+    place: 'royal mg residence, dhamar yone street, hlaing, yangon, myanmar',
+    contact: [
+      'https://studioclip.video',
+      '+09 88413 9136'
+    ]
+  }
+}
