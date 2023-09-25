bef18cc177e8044d0ac9450fadc68722cb56eda0
곽윤호 <kwakdm95@gmail.com>
2023-09-25 16:42:50 +0630

    add: add page property

--- client/src/utils/SelectLanguage.tsx
+++ client/src/utils/SelectLanguage.tsx
@@ -2,21 +2,23 @@ import * as React from "react";
 import { language } from "@/models/language";
 import Link from "next/link";
 
-type Props = React.ComponentProps<"div">;
+type Props = React.ComponentProps<"div"> & {
+  page: string
+};
 
-const SelectLanguage: React.FC<Props> = ({ className }) => {
+const SelectLanguage: React.FC<Props> = ({ className, page }) => {
   return (
     <div
-      className={`${className} flex flex-col justify-center items-center text-center mt-4 md:mt-7 w-10/12`}
+      className={`${className} flex flex-col justify-center items-center text-center mt-4 md:mt-7`}
     >
-      <p className={"p-2 md:p-4 w-full border-b-2 border-content-text"}>
+      <p className={"w-full border-b-2 border-content-text font-bold text-xl p-2"}>
         Language
       </p>
-      <div className={"grid md: grid-cols-3"}>
+      <div className={"flex"}>
         {language.map((lang) => {
           return (
             <React.Fragment key={lang.type}>
-              <Link href={`/about/view/${lang.type}`}>
+              <Link href={`/${page}/view/${lang.type}`}>
                 <p className={"p-2 md:p-4"}>{lang.langName}</p>
               </Link>
             </React.Fragment>
