17feaf9bf6142e5ef9d987fd4d65e0afb13582a0
곽윤호 <kwakdm95@gmail.com>
2023-09-27 11:48:05 +0630

    setting: firebase setting

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: process.env.FIREBASE_STORAGE_URL
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
