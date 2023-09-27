dbf50e8fdfc91412913e03f73af96bc7cbe2b8a2
곽윤호 <kwakdm95@gmail.com>
2023-09-27 11:49:04 +0630

    fix: change loading component position

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -7,12 +7,8 @@ import loadingAniFile from "@pub/loadingAni.json";
 
 const Loading: React.FC = () => {
   return (
-    <div className={"w-full h-full bg-white/50"}>
-      <div
-        className={
-          "flex justify-center items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-        }
-      >
+    <div className={"w-full h-full bg-white/50 relative flex justify-center items-center"}>
+      <div>
         <Lottie
           loop
           play
