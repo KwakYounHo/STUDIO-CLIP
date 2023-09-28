c19643821792c3737a5e0cf0cfbc906559d39de9
곽윤호 <kwakdm95@gmail.com>
2023-09-28 18:51:19 +0630

    add: add layout page(about, work)

---
+++ client/src/app/about/layout.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import SelectLanguage from "@/utils/SelectLanguage";
+
+const AboutLayout = async ({
+  children,
+}: {
+  children: React.ReactNode;
+}): Promise<React.JSX.Element> => {
+  return (
+    <main
+      className={
+        "flex flex-col gap-10 items-center justify-center text-content-text"
+      }
+    >
+      <SelectLanguage page={"about"} className={"w-10/12"} />
+      {children}
+    </main>
+  );
+};
+export default AboutLayout;
