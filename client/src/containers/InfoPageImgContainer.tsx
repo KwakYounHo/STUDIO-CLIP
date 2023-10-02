7bb4aed74a024669f2dec6e65bc7d64e18f81e5f
곽윤호 <kwakdm95@gmail.com>
2023-10-02 12:57:49 +0630

    apply: apply prettier

--- client/src/containers/InfoPageImgContainer.tsx
+++ client/src/containers/InfoPageImgContainer.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 
-import getDownloadImgURL, { GetURLParams } from "@/utils/infoPageImgGetDownloadURL";
+import getDownloadImgURL, {
+  GetURLParams,
+} from "@/utils/infoPageImgGetDownloadURL";
 import InfoPageImgRenderer from "@/components/InfoPageImgRenderer";
 
 const InfoPageImgContainer = async ({
   pages,
-  lang
+  lang,
 }: GetURLParams): Promise<React.JSX.Element> => {
   const items = await getDownloadImgURL({ pages, lang });
   return (
