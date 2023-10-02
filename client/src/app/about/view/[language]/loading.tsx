1505121a10cff123606de0e7dc5ab7f0614bfa0f
곽윤호 <kwakdm95@gmail.com>
2023-10-02 14:45:31 +0630

    delete: delete dummy data

--- client/src/app/about/view/[language]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import Loading from "@/app/loading";
-
-const AboutViewLoading: React.FC = () => {
-  return <Loading />;
-};
-export default AboutViewLoading;
--- client/src/app/work/view/[language]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import Loading from "@/app/loading";
-
-const WorkViewLoading: React.FC = () => {
-  return <Loading />;
-};
-export default WorkViewLoading;
