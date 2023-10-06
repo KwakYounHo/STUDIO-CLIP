37156e6bcd78e02ae65a0b22367419d359bb9720
곽윤호 <kwakdm95@gmail.com>
2023-10-06 14:22:28 +0630

    add: add tailwind css config add about.css file

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,30 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        studioBackground: 'black',
+        studioBackground: "black",
         menu: {
-          bg: '#d8d8d8',
-          text: 'black'
+          bg: "#d8d8d8",
+          text: "black",
         },
         content: {
-          text: 'white'
+          text: "white",
         },
-      }
+      },
+      fontFamily: {
+        appleSD: ["AppleSDGothicNeo", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
