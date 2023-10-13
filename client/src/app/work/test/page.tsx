b626be806a2c107f49d27aa68ddeb8e699370f09
곽윤호 <kwakdm95@gmail.com>
2023-10-13 15:41:45 +0630

    fix: font file change (impact)

--- client/src/app/work/test/page.tsx
+++ client/src/app/work/test/page.tsx
@@ -14,7 +14,9 @@ const Work: React.FC = () => {
       <div className="relative w-full p-7 flex flex-col justify-center items-center">
         <div className="relative w-full h-full flex flex-col justify-center items-center">
           <div id="title" className={"w-full"}>
-            <h1 className={`${impact.className} text-7xl my-10`}>work</h1>
+            <h1 className={`${impact.className} text-7xl my-10 uppercase`}>
+              work
+            </h1>
           </div>
           <div className={"bg-page020408 bg-bottom bg-auto relative mb-10"}>
             <div className={"w-full bg-black/70 absolute inset-0"} />
