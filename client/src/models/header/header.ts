8410153f4792cc6e3ba170c62144eaef645dabb0
곽윤호 <kwakdm95@gmail.com>
2024-01-08 13:40:55 +0630

    change: change social icon position

--- client/src/models/header/header.ts
+++ client/src/models/header/header.ts
@@ -1,94 +1,94 @@
 export const header = {
   nav: [
     {
-      id: 'about',
-      title: 'about us',
-      url: '/about'
+      id: "about",
+      title: "about us",
+      url: "/about",
     },
     {
-      id: 'ourWork',
-      title: 'our work',
+      id: "ourWork",
+      title: "our work",
       subTitle: [
         {
-          title: 'create content',
-          url: '#'
+          title: "create content",
+          url: "#",
         },
         {
-          title: 'promotional video',
-          url: '#'
+          title: "promotional video",
+          url: "#",
         },
         {
-          title: 'Event',
-          url: '#'
+          title: "Event",
+          url: "#",
         },
         {
-          title: 'solution',
-          url: '#'
-        }
+          title: "solution",
+          url: "#",
+        },
       ],
-      url: '/work'
+      url: "/work",
     },
     {
-      id: 'portfolio',
-      title: 'portfolio',
+      id: "portfolio",
+      title: "portfolio",
       subTitle: [
         {
-          title: 'entertainment',
-          url: '#'
+          title: "entertainment",
+          url: "#",
         },
         {
-          title: 'public associations / company',
-          url: '#'
+          title: "public associations / company",
+          url: "#",
         },
         {
-          title: 'interview',
-          url: '#'
+          title: "interview",
+          url: "#",
         },
         {
-          title: 'Event',
-          url: '#'
-        }
+          title: "Event",
+          url: "#",
+        },
       ],
-      url: '/portfolio'
+      url: "/portfolio",
     },
     {
-      id: 'contact',
-      title: 'contact us',
-      url: '/contact'
+      id: "contact",
+      title: "contact us",
+      url: "/contact",
     },
   ],
   social: [
     {
-      id: 'instargram',
-      blackIcon: '/insta_50px.svg',
-      colorIcon: '/insta_color.svg',
+      id: "facebook",
+      blackIcon: "/facebook_50px.svg",
+      colorIcon: "/facebook_color.svg",
       width: 50,
       height: 50,
-      url: 'https://www.instagram.com/_studio_clip/'
+      url: "https://www.facebook.com/studioclip.Official",
     },
     {
-      id: 'facebook',
-      blackIcon: '/facebook_50px.svg',
-      colorIcon: '/facebook_color.svg',
+      id: "instargram",
+      blackIcon: "/insta_50px.svg",
+      colorIcon: "/insta_color.svg",
       width: 50,
       height: 50,
-      url: 'https://www.facebook.com/studioclip.Official'
+      url: "https://www.instagram.com/_studio_clip/",
     },
     {
-      id: 'youtube',
-      blackIcon: '/youtube_50px.svg',
-      colorIcon: '/youtube_color.svg',
+      id: "youtube",
+      blackIcon: "/youtube_50px.svg",
+      colorIcon: "/youtube_color.svg",
       width: 50,
       height: 50,
-      url: 'https://www.youtube.com/@StudioClip_'
+      url: "https://www.youtube.com/@StudioClip_",
     },
     {
-      id: 'tiktok',
-      blackIcon: '/tiktok_50px.svg',
-      colorIcon: '/tiktok_color.svg',
+      id: "tiktok",
+      blackIcon: "/tiktok_50px.svg",
+      colorIcon: "/tiktok_color.svg",
       width: 50,
       height: 50,
-      url: 'https://www.tiktok.com/@studio.clip'
+      url: "https://www.tiktok.com/@studio.clip",
     },
-  ]
-}
+  ],
+};
