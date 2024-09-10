65d1196204251999a76b6dc8bc6e19d88cbf5940
곽윤호 <kwakdm95@gmail.com>
2024-09-10 15:45:36 +0900

    change: change company's address

--- client/src/models/footer/footer.ts
+++ client/src/models/footer/footer.ts
@@ -1,20 +1,20 @@
 export const footer = {
   description: {
-    title: 'STUDIO CLIP Corp.',
-    place: 'royal mg residence, dhamar yone street, hlaing, yangon, myanmar',
+    title: "STUDIO CLIP Corp.",
+    place: "dhama thukha kyaung ST, yangon, myanmar",
     contact: [
       {
-        id: 'webpage',
-        url: 'https://studioclip.video'
+        id: "webpage",
+        url: "https://studioclip.video",
       },
       {
-        id: 'facebook',
-        url: 'https://www.facebook.com/studioclip.Official'
+        id: "facebook",
+        url: "https://www.facebook.com/studioclip.Official",
       },
       {
-        id: 'tel',
-        num: '09 88413 9136'
-      }
-    ]
-  }
-}
+        id: "tel",
+        num: "09 88413 9136",
+      },
+    ],
+  },
+};
